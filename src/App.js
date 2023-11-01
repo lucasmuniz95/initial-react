fa8699582ee1f41148d48012dbe459f044cd77e4
lucasmuniz95 <andradelucasmuniz@gmail.com>
2023-11-01 19:30:17 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Card from './componentes/layout/Card';
+import Pai from './componentes/familia/Pai';
+import Padrasto from './componentes/familia/Padrasto';
+import Filho from './componentes/familia/Filho';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default () => {
+
+    return (
+        <div className="App">
+            <h1>Atividade Fundamentos React</h1>
+            <div className="Cards">
+                <Card titulo="Passando Props do Pai para o Filho" color='#8B0000'>
+                    <Pai sobrenome="JavaScript"/>
+                </Card>
+                <Card titulo="Sem o Pai" color='#4B0082'>
+                    <Padrasto sobrenome="React">
+                        <Filho nome="Chamando Filho 1"/>
+                        <Filho nome="Chamando Filho 2"/>
+                        <Filho nome="Chamando Filho 3"/>
+                    </Padrasto>
+                </Card>
+                <Card titulo="Sem o Pai sem props" color='#008080'>
+                        <Filho nome="Chamando Filho 1"/>
+                        <Filho nome="Chamando Filho 2"/>
+                        <Filho nome="Chamando Filho 3"/>
+                </Card>
+            </div>
+            
+        </div>
+    )
+
 }
-
-export default App;
